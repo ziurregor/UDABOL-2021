cb80b1c9ce72a28d2679db16ef74bb8091e04cbf
Jhonatan Paredes Equise <hjhonatan2016@gmail.com>
2021-10-05 11:42:13 -0400

    pacientes

--- clase3/routes/web.php
+++ clase3/routes/web.php
@@ -20,7 +20,7 @@ Route::get('/', function () {
 Auth::routes();
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 Route::resource('paciente', 'App\Http\Controllers\pacienteController');
-<<<<<<< HEAD
+
 
 //Route::get('/pdf', 'PDFController@PDF')->name('descargarPDF');
 Route::get('/pdf', 'App\Http\Controllers\PDFController@PDF')->name('descargarPDF');
@@ -28,8 +28,7 @@ Route::get('/pdf', 'App\Http\Controllers\PDFController@PDF')->name('descargarPDF
 Route::get('/pdfpacientes', 'App\Http\Controllers\PDFController@PDFPacientes')->name('descargarPDFPacientes');
 
 
-=======
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
->>>>>>> c56ac2f49ea04793ec5199507226597fc6a94a3d
+
